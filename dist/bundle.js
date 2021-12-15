ad15cc19b54550ab65c1decfd958a145f7a5c51a
Janaína Scher <janaina.scher@gmail.com>
2021-12-14 21:57:51 -0300

    atualizacao

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,9 @@
-const user = {
-  name: "Diego"
-};
-console.log(user.address?.street);
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
