de7026e60f8483367650adff6053aa2674ea7e47
Janaína Scher <janaina.scher@gmail.com>
2021-12-18 22:32:18 -0300

    adicionado plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: path.resolve(__dirname, "src", "index.jsx"),
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     resolve: {
-        extensions: [".js", ".jsx"]
+        extensions: [".js", ".jsx"],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        })
+    ],
     module: {
         rules: [
             {
