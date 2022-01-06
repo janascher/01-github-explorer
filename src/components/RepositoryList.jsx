71da2384a2b40e9ed0ba0ea6f4b5c7722c572c6b
Janaína Scher <janaina.scher@gmail.com>
2022-01-06 14:50:01 -0300

    lista os repositorios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,31 +2,25 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 
-const repository = {
-    name:"unform",
-    description:"Forms in React",
-    link: "https://github.com/unform//unform"
-}
-
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState();
+    const [repositories, setRepositories] = useState([]);
 
     useEffect (() => {
         fetch("https://api.github.com/orgs/rocketseat/repos")
         .then(response => response.json())
         .then(data => setRepositories(data))
-    }, []) // toda vez que repositories mudar, a função dentro do useEffect será executada
+    }, []); // toda vez que repositories mudar, a função dentro do useEffect será executada
 
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
-            <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-            </ul>
+        <ul>
+            {repositories.map((repository) => {
+              return <RepositoryItem repository={repository} />
+            })}
+        </ul>
+
         </section>
     );
 }
